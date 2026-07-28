9841676f446dd7b54e65b541ee50e37f128b1d6c
ESTELA RUFINO BRITO <25203670@dsn127m1241953.cfp127aluno.local>
2026-07-28 16:16:54 -0300

    Finalizado a criação e a estilização da primeira tela

--- js/curso.js
+++ js/curso.js
@@ -1,8 +1,20 @@
 import { getCursos } from "./methods.js";
 
-export const criarPagina = function(){
-    const main = document.getElementById('main')
+const caregarBotao = async function(){
+    const dados = await getCursos()
+    if(dados){
+        criarPagina(dados)
+    }
+    else
+        return "Erro ao pegar dados"
+}
+
+export const criarPagina = async function(cursos){
+    let main = document.getElementById('main')
     main.replaceChildren()
+
+    let divPrincipal = document.createElement('div')
+    divPrincipal.className = 'principal'
 
     const divEscolhaCurso = document.createElement('div')
     divEscolhaCurso.className = 'escolhaCurso'
@@ -16,11 +28,33 @@ export const criarPagina = function(){
     // Para poder colocar algo dentro do outro em um texto adicione append e em seguida compacte 
     h1EscolhaCurso.append('Escolha um ', spanCurso, ' para gerenciar')
     
-    
     const imgComputador = document.createElement('img')
-    // Colocar caminho
-    imgComputador.src = './img/logo-image.png'
+    imgComputador.src = './img/devices.svg'
+
+
+    const imgMulher = document.createElement('img')
+    imgMulher.src = './img/studant.svg'
+    imgMulher.className = 'imgMulher'
+    let divButoes = document.createElement('div')
+    divButoes.className = 'botoes'
+
+    cursos.forEach(buscaCursos => {
+        let bntCursos = document.createElement('button')
+        bntCursos.className = 'bntCursos'
+        
+        let imgIcon = document.createElement('img')
+        imgIcon.src = buscaCursos.icon
+
+        let pSigla = document.createElement('p')
+        pSigla.textContent = buscaCursos.sigla
+
+        bntCursos.append(imgIcon,pSigla)
+        divButoes.append(bntCursos)
+    })
 
     divEscolhaCurso.append(h1EscolhaCurso,imgComputador)
-    main.append(divEscolhaCurso)
+    divPrincipal.append(divEscolhaCurso,imgMulher,divButoes)
+    main.appendChild(divPrincipal)
 }
+
+caregarBotao()
--- js/methods.js
+++ js/methods.js
@@ -1,7 +1,10 @@
+'use strict'
+
 const url = 'https://lion-school-phbo.onrender.com/'
 
 export async function getCursos(){
-    const response = await fetch(`${url}/cursos`)
+    const response = await fetch(`${url}cursos`)
+    const data = await response.json()
+    return data
 
-    if(!response.ok) throw new Error(`Erro ao puxar os cursos cadastrados!!`)
 }
